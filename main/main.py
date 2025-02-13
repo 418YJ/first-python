66f772dca15feae3b67abfcf26aa95fecb8e0c40
Jeonyeongjin <yjj9504@gmail.com>
2025-02-13 10:39:18 -0800

    TIL

--- main/main.py
+++ main/main.py
@@ -1,3 +1,23 @@
 a=int(input('나이를 입력하세요.'))
 print(a)
 print(a+a)
+
+print(round(3.89,1))
+print(pow(3,2))
+
+x,y=divmod(7,2)
+print(x) #몫
+print(y) #나머지
+
+#최대값
+print(max([7,5,1,3]))
+
+#최소값
+print(min(7,5,1,3))
+
+#집합도 되고 그냥해도됨, 합은 집합만됨! 배열이라고 해야할지 집합이라고 해야할지 모르겠음.
+
+#합
+print(sum([7,5,1,3]))
+
+
